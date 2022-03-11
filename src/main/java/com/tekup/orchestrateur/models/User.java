b5d4b35dd64cdaf9708427de675bb3b803a61410
wbenromdhane <wbenromdhane@ITEM-S39524.emea.msad.sopra>
2022-03-11 13:40:12 +0100

    first commit

--- src/main/java/com/tekup/orchestrateur/models/User.java
+++ src/main/java/com/tekup/orchestrateur/models/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table (name= "Users")
-public class User { 
+public class User {
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
